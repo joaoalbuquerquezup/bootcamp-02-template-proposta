14784d201030073cdde10fe6076e5ca935d86759
Joao Albuquerque <joao.albuquerque@zup.com.br>
2020-11-27 15:34:01 -0300

    feat (main): add uncaught exception handler and constraintViolation handler

--- src/main/java/br/com/zup/proposta/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/zup/proposta/exception/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package br.com.zup.proposta.exception;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,15 +16,20 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     private final MessageSource messageSource;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    private static final String GENERIC_ERROR = "Ocorreu um erro inesperado no sistema. Tente mais tarde";
 
     public GlobalExceptionHandler(MessageSource messageSource) {
         this.messageSource = messageSource;
@@ -34,18 +42,27 @@ public class GlobalExceptionHandler {
         List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
 
         return fieldErrors.stream().map(fieldError -> {
-            String message = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+            String message = this.messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
             return new ApiError(fieldError.getField(), message);
         }).collect(Collectors.toList());
     }
 
-    @ResponseStatus (code = UNPROCESSABLE_ENTITY)
-    @ExceptionHandler (ConstraintViolationException.class)
-    public List<ApiError> handleInvalidState (ConstraintViolationException exception) {
+    @ResponseStatus(code = UNPROCESSABLE_ENTITY)
+    @ExceptionHandler(ConstraintViolationException.class)
+    public List<ApiError> handleInvalidState(ConstraintViolationException exception) {
         Set<ConstraintViolation<?>> constraintViolations = exception.getConstraintViolations();
         return constraintViolations
                 .stream()
-                .map(e -> new ApiError(e.getPropertyPath().toString(), e.getMessage()))
+                .map(violation -> new ApiError(violation.getPropertyPath().toString(), violation.getMessage()))
                 .collect(Collectors.toList());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> handleUncaught(Exception ex) {
+        LOGGER.error(ex.getMessage(), ex);
+
+        Map<String, String> ERROR = Map.of("message", GlobalExceptionHandler.GENERIC_ERROR);
+
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(ERROR);
     }
 }
