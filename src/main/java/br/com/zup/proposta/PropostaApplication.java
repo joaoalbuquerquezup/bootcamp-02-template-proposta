e660b0291fd094e55eab8e6e11161ba0cb246e09
Joao Albuquerque <joao.albuquerque@zup.com.br>
2020-11-27 11:08:40 -0300

    style (main): change package name

--- src/main/java/br/com/zup/proposta/PropostaApplication.java
+++ src/main/java/br/com/zup/proposta/PropostaApplication.java
@@ -1,4 +1,4 @@
-package br.com.zup.proposta.proposta;
+package br.com.zup.proposta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/br/com/zup/proposta/PropostaApplicationTests.java
+++ src/test/java/br/com/zup/proposta/PropostaApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.zup.proposta.proposta;
+package br.com.zup.proposta;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
