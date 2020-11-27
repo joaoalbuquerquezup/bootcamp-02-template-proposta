2c1692737e5c74b040d2c56addd2f1ef7ac812d1
Joao Albuquerque <joao.albuquerque@zup.com.br>
2020-11-27 15:33:00 -0300

    feat (main): add unique constraint and unique validation at proposal creation

--- src/main/java/br/com/zup/proposta/NewProposalRequest.java
+++ src/main/java/br/com/zup/proposta/NewProposalRequest.java
@@ -1,6 +1,7 @@
 package br.com.zup.proposta;
 
 import br.com.zup.proposta.validation.CpfCnpj;
+import br.com.zup.proposta.validation.Unique;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
@@ -14,7 +15,7 @@ import java.math.BigDecimal;
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class NewProposalRequest {
 
-    @NotBlank @CpfCnpj
+    @NotBlank @CpfCnpj @Unique(clazz = Proposal.class)
     private String document;
 
     @NotBlank @Email
