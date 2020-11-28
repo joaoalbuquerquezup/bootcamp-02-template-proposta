3cec7c1df654f0b58d2a6ea15fd44ec6bdd9808a
Joao Albuquerque <joao.albuquerque@zup.com.br>
2020-11-28 05:45:13 -0300

    feat (main): add static ObjectMapper

---
+++ src/main/java/br/com/zup/proposta/utils/StaticUtils.java
@@ -0,0 +1,11 @@
+package br.com.zup.proposta.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+
+public class StaticUtils {
+
+    // TODO: Trocar para um ObjectPool
+    public static final ObjectMapper JSON_MAPPER = new ObjectMapper();
+
+}
