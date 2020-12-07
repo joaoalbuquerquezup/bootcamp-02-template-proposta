92e93e48a095ccca4dee1155dbeb3dc160488d15
Joao Albuquerque <joao.albuquerque@zup.com.br>
2020-12-07 18:07:39 -0300

    feat (main): add retrieve card by id with biometry

---
+++ src/main/java/br/com/zup/proposta/card/CardRepository.java
@@ -0,0 +1,16 @@
+package br.com.zup.proposta.card;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface CardRepository extends JpaRepository<Card, UUID> {
+
+    @EntityGraph(attributePaths = { "biometryList" })
+    Optional<Card> getById(UUID id);
+
+}
