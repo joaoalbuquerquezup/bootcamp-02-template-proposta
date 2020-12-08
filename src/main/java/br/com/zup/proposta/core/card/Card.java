6b0cef831e752100550ad636ccecc00cee4e7961
Joao Albuquerque <joao.albuquerque@zup.com.br>
2020-12-08 14:43:13 -0300

    feat (main): manually checking the resource owner

--- src/main/java/br/com/zup/proposta/core/card/Card.java
+++ src/main/java/br/com/zup/proposta/core/card/Card.java
@@ -2,17 +2,20 @@ package br.com.zup.proposta.core.card;
 
 import br.com.zup.proposta.core.card.biometry.Biometry;
 import br.com.zup.proposta.core.card.lock.BlockCard;
+import br.com.zup.proposta.core.proposal.Proposal;
 import br.com.zup.proposta.utils.AssertWithHttpStatus;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -36,6 +39,10 @@ public class Card {
     @OneToOne(cascade = CascadeType.PERSIST)
     private BlockCard blockCard;
 
+    @NotNull
+    @OneToOne(optional = false, fetch = FetchType.LAZY, mappedBy = "card")
+    private Proposal proposal;
+
     /**
      * Hibernate usage only
      */
@@ -53,5 +60,9 @@ public class Card {
     public void block(BlockCard blockCard) {
         AssertWithHttpStatus.isNull(this.blockCard, UNPROCESSABLE_ENTITY);
         this.blockCard = blockCard;
+    }
+
+    public Proposal getProposal() {
+        return proposal;
     }
 }
